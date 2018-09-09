5afbe8950e4f6f104d0fe40f086b2aaadb5c8365
ardeezstyle <ardeezstyle@gmail.com>
2018-09-09 23:16:56 +0530

    Setting up repo

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ExamModule } from './exam/exam.module';
+import { ProductRoutingModule } from './product/product-routing.module';
+import { ProductModule } from './product/product.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { NotFoundService } from './services/not-found.service';
+import {HeaderComponent} from "./header/header.component";
+import {NavigationComponent} from "./header/navigation/navigation.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    DashBoardComponent,
+    HeaderComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ProductRoutingModule,
+    ExamModule,
+    ProductModule,
+    // the root routing module must on the end of this array
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [NotFoundService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
